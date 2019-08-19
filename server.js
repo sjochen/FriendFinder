90497db2b262a988b129b6aba629cead44e9c303
Stephen Jochen <stephenjochen2@gmail.com>
2019-08-19 17:58:36 -0400

    got the app working but having issues getting the modal to pop up

--- server.js
+++ server.js
@@ -2,21 +2,15 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
-require("./app/routing/apiRoutes")
-require("./app/routing/htmlRoutes")
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/app/public/home.html"));
-});
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
-app.get("/friends", function(req, res) {
-  res.sendFile(path.join(__dirname, "/app/public/friends.js"));
-});
+
 
 app.listen(PORT, function() {
     console.log("Server is listening on PORT: " + PORT);
